2872f2c157f8f8028f9c7aa30fe1575c722e0c32
Artur Santiago <artursantiago2001@hotmail.com>
2020-01-20 17:39:56 -0300

    add result and btns areas

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 new Vue({
     el: '#app',
     data: {
-
+        running: false,
+        knightLife: 100,
+        dragonLife: 0,
     },
     computed: {
-
+        hasResult() {
+            return this.knightLife == 0 || this.dragonLife == 0
+        }
     },
     methods: {
 
